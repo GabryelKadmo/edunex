7a796b9576780bd7fc26b0eca9791814afc4ca04
Bruno Arouca <brunoarouca1@gmail.com>
2024-11-05 17:53:15 -0300

    fix hover link sidebar

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import HomeIcon from "@/components/icons/home-icon";
-import { CustomIconType } from "@/types/custom-icon";
-import Image from "next/image";
 import CursoIcon from "@/components/icons/curso-icon";
 import DicasIcon from "@/components/icons/dicas-icon";
 import CurriculoIcon from "@/components/icons/curriculo-icon";
+import { CustomIconType } from "@/types/custom-icon";
+import Image from "next/image";
 
 type NavItemType = {
   text: string;
@@ -50,7 +50,7 @@ export const Sidebar = () => {
   return (
     <div className="px-6 bg-white w-full h-full border-r border-black/10">
       <div className="py-6 border-b border-primary-50 flex justify-center">
-        <Image alt="Logo Edunex" src={"/logo.svg"} height={30} width={190}/>
+        <Image alt="Logo Edunex" src={"/logo.svg"} height={30} width={190} />
       </div>
       <ul className="py-6 space-y-2">
         {navItems.map((item, index) => {
@@ -59,17 +59,25 @@ export const Sidebar = () => {
             <li key={index} className="relative">
               <Link
                 href={item.href}
-                className={`flex items-center gap-3 px-3 py-2.5 relative z-10 ${isActive ? "bg-primary-50 rounded-md text-primary" : "text-gray-500"} `}
+                className={`flex items-center gap-3 px-3 py-2.5 rounded-md relative z-10 ${
+                  isActive
+                    ? "bg-primary-50 text-primary"
+                    : "hover:bg-primary-300 hover:text-white text-gray-500"
+                }`}
               >
                 <item.icon
                   variant={"outline"}
                   width={18}
                   height={18}
                   fill="currentColor"
-                  className="mb-1"
+                  className={`mb-1 ${
+                    isActive ? "text-primary" : "hover:text-white"
+                  }`}
                 />
                 <span
-                  className={isActive ? "text-primary font-medium text-sm" : "text-sm font-medium text-gray-500"}
+                  className={`text-sm font-medium hover:text-white${
+                    isActive ? "text-primary" : "text-gray-500"
+                  }`}
                 >
                   {item.text}
                 </span>
