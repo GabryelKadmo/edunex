73a973cb3ecc44b1193d017dea8d200491eefa91
Bruno Arouca <brunoarouca1@gmail.com>
2024-11-09 19:25:44 -0300

    feat: add section members

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { CardCourse } from "@/components/card-course";
+import { CardHorizontal } from "@/components/card-horizontal";
 import MenuIcon from "@/components/icons/menu-icon";
 import { ListAreas } from "@/components/list-areas";
+import Members from "@/components/members";
 import { SearchInput } from "@/components/search-input";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { courses } from "@/data/courses";
@@ -36,18 +38,83 @@ export default function Home() {
       </section>
       <section>
         <div className="px-5 flex items-center justify-between mb-2">
-          <h2 className="font-semibold text-xl text-gray-800">Cursos Populares</h2>
-          <Link className="font-semibold text-primary" href={"/cursos"}>Ver todos</Link>
+          <h2 className="font-semibold text-xl text-gray-800">
+            Cursos Populares
+          </h2>
+          <Link className="font-semibold text-primary" href={"/cursos"}>
+            Ver todos
+          </Link>
         </div>
         <div>
           <ScrollArea className="w-full whitespace-nowrap">
             <div className="flex w-full space-x-4 p-4">
               {courses.map((course) => (
-                <CardCourse key={course.id}/>
+                <CardCourse key={course.id} />
               ))}
             </div>
             <ScrollBar orientation="horizontal" />
           </ScrollArea>
+        </div>
+      </section>
+      <section className="mt-11 px-5 ">
+        <div className="flex items-center justify-between mb-7">
+          <h2 className="font-semibold text-xl text-gray-800">Dicas</h2>
+          <Link className="font-semibold text-primary" href={"/dicas"}>
+            Ver todas
+          </Link>
+        </div>
+        <div>
+          <div className="flex flex-col w-full gap-8">
+            {courses.map((course) => (
+              <CardHorizontal key={course.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="px-5 py-11">
+        <div
+          className="relative h-[580px] bg-cover bg-center rounded-2xl"
+          style={{ backgroundImage: "url('/bg-about.png')" }}
+        >
+          <div className="absolute inset-0 flex flex-col items-center text-white text-center px-4 py-16">
+            <div className="mt-1">
+              <Image
+                alt="Polegar"
+                src={"/polegar.png"}
+                width={30}
+                height={30}
+              />{" "}
+            </div>
+            <h2 className="text-2xl font-semibold text-neutral-700">
+              Sobre nós
+            </h2>
+            <p className="mt-12 text-center text-neutral-500">
+              Somos um grupo de alunos da Faculdade Unex, unidos pelos cursos de
+              Administração e Sistemas de Informação, com o objetivo de criar
+              uma solução que impactasse positivamente a vida das pessoas.
+              Acreditamos que o acesso à educação e ao desenvolvimento
+              profissional deve ser acessível a todos, e foi com essa visão que
+              desenvolvemos nossa plataforma.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="px-5">
+        <div
+          className="relative h-[150px] bg-cover bg-center rounded-2xl flex items-center justify-center"
+          style={{ backgroundImage: "url('/bg-our-team.png')" }}
+        >
+          <div className="absolute inset-0 flex gap-2 items-center justify-center text-white text-center">
+            <h2 className="text-2xl font-semibold text-primary-200">
+              Nossa Equipe
+            </h2>
+            <Image alt="Polegar" src={"/coracao.png"} width={30} height={30} />
+          </div>
+        </div>
+        <div className="mt-12">
+          <Members />
         </div>
       </section>
     </>
---
+++ src/data/members.ts
@@ -0,0 +1,94 @@
+import { Member } from "@/types/member";
+
+export const members: Member[] = [
+  {
+    id: 1,
+    name: "Ana Clara",
+    image: "https://randomuser.me/api/portraits/women/1.jpg",
+    position: "Desenvolvedor"
+  },
+  {
+    id: 2,
+    name: "Bruno Henrique",
+    image: "https://randomuser.me/api/portraits/men/2.jpg",
+    position: "Marketing"
+  },
+  {
+    id: 3,
+    name: "Carla Maria",
+    image: "https://randomuser.me/api/portraits/women/3.jpg",
+    position: "Designer"
+  },
+  {
+    id: 4,
+    name: "Daniel Augusto",
+    image: "https://randomuser.me/api/portraits/men/4.jpg",
+    position: "Marketing"
+  },
+  {
+    id: 5,
+    name: "Eduarda Pires",
+    image: "https://randomuser.me/api/portraits/women/5.jpg",
+    position: "Curador de conteudo"
+  },
+  {
+    id: 6,
+    name: "Fernando Silva",
+    image: "https://randomuser.me/api/portraits/men/6.jpg",
+    position: "Desenvolvedor"
+  },
+  {
+    id: 7,
+    name: "Gabriela Oliveira",
+    image: "https://randomuser.me/api/portraits/women/7.jpg",
+    position: "Marketing"
+  },
+  {
+    id: 8,
+    name: "Henrique Gomes",
+    image: "https://randomuser.me/api/portraits/men/8.jpg",
+    position: "Marketing"
+  },
+  {
+    id: 9,
+    name: "Isabela Martins",
+    image: "https://randomuser.me/api/portraits/women/9.jpg",
+    position: "Desenvolvedor"
+  },
+  {
+    id: 10,
+    name: "João Pedro",
+    image: "https://randomuser.me/api/portraits/men/10.jpg",
+    position: "Desenvolvedor"
+  },
+  {
+    id: 11,
+    name: "Karla Dias",
+    image: "https://randomuser.me/api/portraits/women/11.jpg",
+    position: "Curador de conteudo"
+  },
+  {
+    id: 12,
+    name: "Lucas Fernando",
+    image: "https://randomuser.me/api/portraits/men/12.jpg",
+    position: "Marketing"
+  },
+  {
+    id: 13,
+    name: "Mariana Lima",
+    image: "https://randomuser.me/api/portraits/women/13.jpg",
+    position: "Redator"
+  },
+  {
+    id: 14,
+    name: "Nicolas Ribeiro",
+    image: "https://randomuser.me/api/portraits/men/14.jpg",
+    position: "Marketing"
+  },
+  {
+    id: 15,
+    name: "Olivia Fernandes",
+    image: "https://randomuser.me/api/portraits/women/15.jpg",
+    position: "Curador de conteudo"
+  }
+];
