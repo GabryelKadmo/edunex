357b9c989d45a9a014b49a713685117747baa1fb
Bruno Arouca <brunoarouca1@gmail.com>
2024-11-09 16:13:15 -0300

    feat: add Categories and Areas

---
+++ src/data/areas.ts
@@ -0,0 +1,49 @@
+import { Area } from "@/types/area";
+
+export const areas: Area[] = [
+  {
+    id: 1,
+    name: "Tecnologia",
+    icon: "/icons/tecnologia.svg"
+  },
+  {
+    id: 2,
+    name: "Negócios",
+    icon: "/icons/negocios.svg"
+  },
+  {
+    id: 3,
+    name: "Evolução",
+    icon: "/icons/evolucao-pessoal.svg"
+  },
+  {
+    id: 4,
+    name: "Ciências",
+    icon: "/icons/ciencias-humanas.svg"
+  },
+  {
+    id: 5,
+    name: "Saúde",
+    icon: "/icons/saude.svg"
+  },
+  {
+    id: 6,
+    name: "Criatividade",
+    icon: "/icons/criatividade.svg"
+  },
+  {
+    id: 7,
+    name: "Idiomas",
+    icon: "/icons/idiomas.svg"
+  },
+  {
+    id: 8,
+    name: "Marketing",
+    icon: "/icons/marketing.svg"
+  },
+  {
+    id: 9,
+    name: "Outros",
+    icon: "/icons/marketing.svg"
+  },
+]
---
+++ src/data/categories.ts
@@ -0,0 +1,39 @@
+import { Category } from "@/types/Category";
+import { areas } from "./areas";
+
+export 
+const categories: Category[] = [
+  { id: 1, name: 'Desenvolvimento Web', area: areas[0] },
+  { id: 2, name: 'Inteligência Artificial', area: areas[0] },
+  { id: 3, name: 'Ciência de Dados', area: areas[0] },
+  { id: 4, name: 'Cibersegurança', area: areas[0] },
+  { id: 5, name: 'Empreendedorismo', area: areas[1] },
+  { id: 6, name: 'Gestão de Projetos', area: areas[1] },
+  { id: 7, name: 'Finanças Empresariais', area: areas[1] },
+  { id: 8, name: 'Liderança', area: areas[1] },
+  { id: 9, name: 'Marketing Digital', area: areas[2] },
+  { id: 10, name: 'SEO e SEM', area: areas[2] },
+  { id: 11, name: 'Vendas e Persuasão', area: areas[2] },
+  { id: 12, name: 'Publicidade', area: areas[2] },
+  { id: 13, name: 'Gestão do Tempo', area: areas[3] },
+  { id: 14, name: 'Produtividade', area: areas[3] },
+  { id: 15, name: 'Inteligência Emocional', area: areas[3] },
+  { id: 16, name: 'Comunicação Eficaz', area: areas[3] },
+  { id: 17, name: 'Design Gráfico', area: areas[4] },
+  { id: 18, name: 'UX/UI Design', area: areas[4] },
+  { id: 19, name: 'Edição de Vídeo', area: areas[4] },
+  { id: 20, name: 'Fotografia', area: areas[4] },
+  { id: 21, name: 'Saúde Mental', area: areas[5] },
+  { id: 22, name: 'Nutrição', area: areas[5] },
+  { id: 23, name: 'Exercício Físico', area: areas[5] },
+  { id: 24, name: 'Meditação', area: areas[5] },
+  { id: 25, name: 'Inglês', area: areas[6] },
+  { id: 26, name: 'Espanhol', area: areas[6] },
+  { id: 27, name: 'Francês', area: areas[6] },
+  { id: 28, name: 'Alemão', area: areas[6] },
+  { id: 29, name: 'Psicologia', area: areas[7] },
+  { id: 30, name: 'Sociologia', area: areas[7] },
+  { id: 31, name: 'História', area: areas[7] },
+  { id: 32, name: 'Filosofia', area: areas[7] },
+  { id: 33, name: 'Outros', area: areas[8] }
+];
