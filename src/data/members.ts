870dde32e8407110d465780ad514ad69e4f52f5b
GabryelKadmo <gabryelkadmo@gmail.com>
2025-01-14 04:09:57 -0300

    fix: changed pfp to default in some members

--- src/data/members.ts
+++ src/data/members.ts
@@ -5,138 +5,139 @@ export const members: Member[] = [
     id: 1,
     name: "Bruno Arouca",
     image: "/members/brunoarouca-2.jpg",
-    position: "Desenvolvedor"
+    position: "Desenvolvedor",
   },
   {
     id: 2,
     name: "Gabryel Kadmo",
     image: "/members/kdmo.jpeg",
-    position: "Desenvolvedor"
+    position: "Desenvolvedor",
   },
   {
     id: 3,
     name: "André Lucas",
     image: "/members/andre.jpg",
-    position: "Desenvolvedor"
+    position: "Desenvolvedor",
   },
   {
     id: 4,
     name: "Gustavo Gonzaga",
     image: "/members/gonzaga.jpg",
-    position: "Desenvolvedor"
+    position: "Desenvolvedor",
   },
   {
     id: 5,
     name: "Kamyle Pinheiro",
     image: "/members/Kamyle-Pinheiro.jpg",
-    position: "Marketing"
+    position: "Marketing",
   },
   {
     id: 6,
     name: "Mateus Fellipi",
     image: "/image-perfil.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 7,
     name: "Vinicius Sena",
     image: "/image-perfil.jpg",
-    position: "Marketing"
+    position: "Marketing",
   },
   {
     id: 8,
     name: "Felippe Bonfim",
     image: "/members/felippe.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 9,
     name: "Alberto Duarte",
     image: "/members/Alberto-Duarte.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 10,
     name: "Maria Eduarda",
     image: "/members/Maria-Eduarda.jpeg",
-    position: "Marketing"
+    position: "Marketing",
   },
   {
     id: 11,
     name: "Wellen Souza",
-    image: "https://instagram.fios7-1.fna.fbcdn.net/v/t51.2885-15/66463471_1044123002459427_468752594596495434_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi45NjJ4MTIwMi5zZHIuZjI4ODUuZGVmYXVsdF9pbWFnZSJ9&_nc_ht=instagram.fios7-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=XMobZEmz2pgQ7kNvgF6xqF_&_nc_gid=507f6775e0234a9dafedbc102192077b&edm=APoiHPcBAAAA&ccb=7-5&ig_cache_key=MjA5OTY5NTQwNzQ0OTE4NzAzMQ%3D%3D.3-ccb7-5&oh=00_AYAtU3lLRtZj_3JZYBU90VTa75tUfib0ScCf4yzzt7HZTg&oe=6742B4D7&_nc_sid=22de04",
-    position: "Marketing"
+    image: "/image-perfil.jpg",
+
+    position: "Marketing",
   },
   {
     id: 12,
     name: "Victor Hugo",
     image: "/members/Victor-Hugo.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 13,
     name: "Emanuel Helano",
     image: "/image-perfil.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 14,
     name: "Alberto Luiz",
     image: "/members/Alberto-Luiz.jpeg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 15,
     name: "Fernanda Guedes",
     image: "/members/fernanda-guedes.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 16,
     name: "Jeovana Araújo",
     image: "/members/Jeovana-Araujo.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 17,
     name: "Janaina Silva",
     image: "/members/janaina-silva.jpeg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 18,
     name: "Giulia Caetano",
-    image: "https://media-gru2-1.cdn.whatsapp.net/v/t61.24694-24/418925160_1200247664309734_8731283101874872492_n.jpg?ccb=11-4&oh=01_Q5AaIO2z6SRzSw1PRaJq4f4AxuM4A0CcnS1a0jOpvhbr8gHs&oe=6749F0E7&_nc_sid=5e03e0&_nc_cat=102",
-    position: "Conteúdo"
+    image: "/image-perfil.jpg",
+    position: "Conteúdo",
   },
   {
     id: 19,
     name: "Ryan Lobo",
     image: "/members/Ryan-Lobo.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 20,
     name: "Daniele Pinheiro",
     image: "/members/Daniele-Pinheiro.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 21,
     name: "Mell Rany",
     image: "/members/Mell-Rany.jpg",
-    position: "Marketing"
+    position: "Marketing",
   },
   {
     id: 22,
     name: "Brenda Brandão",
     image: "/members/Brenda-Brandao.jpg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
   {
     id: 23,
     name: "Jonathan da Silva",
     image: "/members/Jonathan-da-Silva.jpeg",
-    position: "Conteúdo"
+    position: "Conteúdo",
   },
 ];
